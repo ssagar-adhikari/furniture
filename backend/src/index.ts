28548debe9b57199a23a064ea1316ce00423f47b
Sagar Adhikari <ssagar1916@gmail.com>
2023-11-22 21:36:28 +0545

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello world!')
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import connectDB from "./db";
+
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//db connection
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("MONGO DB Connection failed");
+  });
